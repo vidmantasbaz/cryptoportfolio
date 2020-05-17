247f68e62138a7a42c780e22155e9dce1c889baa
vidmantasbazevicius <vidmantas.bazevicius@transfergo.com>
2020-05-17 11:16:34 +0300

    add AssetType form tests

--- src/tests/Form/AssetTypeTest.php
+++ src/tests/Form/AssetTypeTest.php
@@ -3,31 +3,37 @@
 
 namespace App\Tests\Form;
 
+use App\Controller\AssetController;
+use App\Entity\Asset;
+use App\Form\AssetType;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\Test\TypeTestCase;
 
 class AssetTypeTest extends TypeTestCase
 {
+
     public function testSubmitValidData()
     {
         $formData = [
-            'test' => 'test',
-            'test2' => 'test2',
+            'label' => 'test',
+            'currency' => 'BTC',
+            'value' => 10,
         ];
 
-        $objectToCompare = new TestObject();
-        // $objectToCompare will retrieve data from the form submission; pass it as the second argument
-        $form = $this->factory->create(TestedType::class, $objectToCompare);
+        $assetTest = new Asset();
+        $form = $this->factory->create(AssetType::class, $assetTest);
 
-        $object = new TestObject();
-        // ...populate $object properties with the data stored in $formData
-
-        // submit the data to the form directly
+        $asset = (new Asset())
+            ->setCurrency('BTC')
+            ->setLabel('test')
+            ->setValue(10);
         $form->submit($formData);
 
         $this->assertTrue($form->isSynchronized());
+        $this->assertTrue($form->isValid());
 
         // check that $objectToCompare was modified as expected when the form was submitted
-        $this->assertEquals($object, $objectToCompare);
+        $this->assertEquals($asset, $asset);
 
         $view = $form->createView();
         $children = $view->children;
@@ -35,6 +41,23 @@ class AssetTypeTest extends TypeTestCase
         foreach (array_keys($formData) as $key) {
             $this->assertArrayHasKey($key, $children);
         }
+    }
+
+    public function testSubmitInvalidData()
+    {
+        $formData = [
+            'label' => null,
+            'currency' => 'asds',
+            'value' => -10,
+        ];
+
+        $assetTest = new Asset();
+        $form = $this->factory->create(AssetType::class, $assetTest);
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertFalse($form->isValid());
     }
 
 }
